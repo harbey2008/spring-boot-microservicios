048f9932b4bd4df4c88fe231d33300e9b4eea274
harbey2008 <harbey2008@hotmail.com>
2021-03-12 16:21:24 -0500

    Inicio Proyecto Micro Servicios

---
+++ src/main/java/com/desarrollo/spring/boot/SpringbootServiciosProductosApplication.java
@@ -0,0 +1,13 @@
+package com.desarrollo.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootServiciosProductosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServiciosProductosApplication.class, args);
+	}
+
+}
